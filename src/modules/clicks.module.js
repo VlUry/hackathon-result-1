2995cdd12cc1be7779596a81d294e39a8fdc5c13
Vladislav Urykov <vladislavurykov29@gmail.com>
2022-10-28 23:18:44 +0300

    clicks module v1

--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,68 +1,74 @@
 import {Module} from '../core/module'
 
 export class ClicksModule extends Module {
-  constructor(time) {
-    super('clicks', `Считать клики (за ${time} секунды)`)
+  constructor(type, text, time) {
+    super(type, text + ` (за ${time} секунд)`)
 
     this.body = document.querySelector('body')
     this.counting = false
     this.time = +time
   }
 
-  #createClicksBlock() {
-    const clicksBlock = document.createElement('div')
-    clicksBlock.className = 'clicksCounter-container'
-    clicksBlock.style.position = 'absolute'
-    clicksBlock.style.top = '1rem'
-    clicksBlock.style.right = '1rem'
-    clicksBlock.style.textAlign = 'end'
-    this.#createTimer(clicksBlock)
-    this.#createClicks(clicksBlock)
-
-    this.body.append(clicksBlock)
-  }
-
-  #createTimer(container) {
+  // Метод для создания таймера
+  #createTimer() {
     const timer = document.createElement('h1')
     timer.className = 'clicksCounter-timer'
-    container.append(timer)
+    timer.style.position = 'absolute'
+    timer.style.top = '1rem'
+    timer.style.right = '1rem'
+    this.body.append(timer)
   }
 
-  #createClicks(container) {
+  // Метод для создания счетчика кликов
+  #createClicks(clicksCount) {
     const clicks = document.createElement('h1')
     clicks.className = 'clicksCounter-clicks'
-    clicks.innerHTML = 'Кликов: 0'
-    container.append(clicks)
+    clicks.innerHTML = `Кликов: ${clicksCount}`
+    clicks.style.position = 'absolute'
+    clicks.style.top = '1rem'
+    clicks.style.right = '1rem'
+    clicks.style.cursor = 'pointer'
+    this.body.append(clicks)
+
+    // Чтобы убрать счетчик, когда не нужен. Можно подоформить, пока идей нет
+    clicks.addEventListener('click', () => clicks.remove())
   }
 
   #counter() {
-    this.#createClicksBlock()
-    const timer = document.querySelector('.clicksCounter-timer')
-    const clicks = document.querySelector('.clicksCounter-clicks')
+    // Первая проверка, чтобы нельзя было вызвать дважды
+    if (!this.counting) {
 
-    document.addEventListener('click', () => {
-      if (!this.counting) {
-        
-        this.counting = true
-        let count = 0
-        let timeNum = this.time
-
-        document.addEventListener('click', () => count++)
-
-        for (let i = 0; i <= this.time; i++) {
-          setTimeout(() => {
-            timer.innerHTML = `0:0${timeNum}`
-            timeNum-- 
-          }, 1000 * i)
-        }
-
-        setTimeout(() => {
-          console.log(count)
-          clicks.innerHTML = `Кликов: ${count}`
-          this.counting = false
-        }, 3000)
+      // Если предыдущий счетчик не убран, убирает
+      if (document.querySelector('.clicksCounter-clicks')) {
+        const clicks = document.querySelector('.clicksCounter-clicks')
+        clicks.remove()
       }
-    })
+
+      this.#createTimer()
+      const timer = document.querySelector('.clicksCounter-timer')
+
+      this.counting = true
+      let count = 0
+      let timeNum = this.time
+
+      // Счетчик кликов
+      document.addEventListener('click', () => count++)
+
+      // Отсчет таймера
+      for (let i = 0; i <= this.time; i++) {
+        setTimeout(() => {
+          timer.innerHTML = `0:0${timeNum}`
+          timeNum-- 
+        }, 1000 * i)
+      }
+
+      // Происходящее, по истечении таймера
+      setTimeout(() => {
+        timer.remove()
+        this.#createClicks(count - 1) // -1 потому, что нажатие на старт таймера идет в счет
+        this.counting = false
+      }, 1000 * timeNum)
+    }
   }
 
   trigger() {
