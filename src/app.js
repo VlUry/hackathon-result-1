63467e14b7aa26a66401619b2be203efe5c17d24
Nikita <tonno651@gmail.com>
2022-10-28 21:46:29 +0300

    develop context-menu

--- src/app.js
+++ src/app.js
@@ -1,2 +1,10 @@
+import { ContextMenu } from './menu'
 import './styles.css'
 
+const menu = new ContextMenu('#menu')
+
+// Добавление модулей в будущее контекстное меню
+// menu.add()
+
+// Формирование html для контекстного меню
+menu.render()
