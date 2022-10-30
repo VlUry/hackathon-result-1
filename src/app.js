0dc607f3b88d098ebb331684e4c517c60c54c02a
Andrey Derunov <derunov.andreyy@gmail.com>
2022-10-30 22:30:48 +0400

    fixed dblclicks

--- src/app.js
+++ src/app.js
@@ -1,21 +1,25 @@
-import "./styles.css"
-import { ContextMenu } from "./menu"
-import { BackgroundModule } from './modules/background.module'
-import { ClicksModule } from './modules/clicks.module'
-import { RandomFigureModule } from "./modules/random-figure.module"
-import { CustomMessageModule } from "./modules/custom.message"
-import { RainingCatsModule } from "./modules/raining-cats.module"
+import "./styles.css";
+import { ContextMenu } from "./menu";
+import { BackgroundModule } from "./modules/background.module";
+import { ClicksModule } from "./modules/clicks.module";
+import { RandomFigureModule } from "./modules/random-figure.module";
+import { CustomMessageModule } from "./modules/custom.message";
+import { RainingCatsModule } from "./modules/raining-cats.module";
 
-const menu = new ContextMenu("#menu")
+const menu = new ContextMenu("#menu");
 
 // Добавление модулей в будущее контекстное меню
 menu.add(
-  new BackgroundModule('bg-color', 'Поменять цвет фона'),
-  new ClicksModule('clicks', 'Считать клики', 3),
+  new BackgroundModule("bg-color", "Поменять цвет фона"),
+  new ClicksModule("clicks", "Считать клики"),
   new RandomFigureModule("create-random", "Создать случайную фигуру"),
-  new CustomMessageModule('custom-message', 'Создать сообщение', 'Всем привет!'),
-  new RainingCatsModule('raining-cats', 'Создать дождь из котов', 15)
-)
+  new CustomMessageModule(
+    "custom-message",
+    "Создать сообщение",
+    "Всем привет!"
+  ),
+  new RainingCatsModule("raining-cats", "Создать дождь из котов", 15)
+);
 
 // Формирование html для контекстного меню
-menu.render()
+menu.render();
--- src/modules/clicks.module.js
+++ src/modules/clicks.module.js
@@ -1,83 +1,94 @@
-import {Module} from '../core/module'
+import { Module } from "../core/module";
 
 export class ClicksModule extends Module {
-  constructor(type, text, time) {
-    super(type, text + ` (за ${time} секунд)`)
+  constructor(type, text, time = 5) {
+    super(type, text + ` (за ${time} сек.)`);
 
-    this.body = document.querySelector('body')
-    this.counting = false
-    this.time = +time
+    this.body = document.querySelector("body");
+    this.counting = false;
+    this.time = +time;
   }
 
   // Метод для создания таймера
   #createTimer() {
-    const timer = document.createElement('h1')
-    timer.className = 'clicks-counter__timer'
-    timer.style.position = 'absolute'
-    timer.style.top = '1rem'
-    timer.style.right = '1rem'
-    return timer
+    const timer = document.createElement("h1");
+    timer.className = "clicks-counter__timer";
+    timer.style.position = "absolute";
+    timer.style.top = "1rem";
+    timer.style.right = "1rem";
+    return timer;
   }
 
   // Метод для создания счетчика кликов
-  #createClicksCounter(clicksCount) {
-    const clicks = document.createElement('div')
-    clicks.className = 'clicks-counter__clicks'
-    clicks.innerHTML = `<h1>Кликов: ${clicksCount}</h1>`
-    clicks.innerHTML += '<h4>Клик, чтобы удалить</h4>'
-    clicks.style.position = 'absolute'
-    clicks.style.top = '1rem'
-    clicks.style.right = '1rem'
-    clicks.style.cursor = 'pointer'
-    this.body.append(clicks)
+  #createClicksCounter(clicksCount, dblClicksCount) {
+    const clicks = document.createElement("div");
+    clicks.className = "clicks-counter__clicks";
+    clicks.innerHTML = `<h2>Кликов: ${clicksCount}</h2>`;
+    clicks.innerHTML += `<h2>Двойных кликов: ${dblClicksCount}</h2>`;
+    clicks.innerHTML += "<h4>Клик, чтобы удалить</h4>";
+    clicks.style.position = "absolute";
+    clicks.style.top = "1rem";
+    clicks.style.right = "1rem";
+    clicks.style.cursor = "pointer";
+    this.body.append(clicks);
 
     // Чтобы убрать счетчик, когда не нужен. Можно подоформить, пока идей нет
-    clicks.addEventListener('click', () => {
-      clicks.remove()
-    })
+    clicks.addEventListener("click", () => {
+      clicks.remove();
+    });
   }
 
   #counter() {
     // Первая проверка, чтобы нельзя было вызвать дважды
     if (!this.counting) {
-
       // Если предыдущий счетчик не убран, убирает
-      const clicksCounter = document.querySelector('.clicks-counter__clicks')
+      const clicksCounter = document.querySelector(".clicks-counter__clicks");
       if (clicksCounter) {
-        clicksCounter.remove()
+        clicksCounter.remove();
       }
 
-      const timer = this.#createTimer()
-      this.body.append(timer)
+      const timer = this.#createTimer();
+      this.body.append(timer);
 
-      this.counting = true
-      let count = 0
-      let timeNum = this.time
+      this.counting = true;
+      let count = 0;
+      let countDbl = 0;
+      let timeNum = this.time;
 
-      // Счетчик кликов
-      document.addEventListener('click', () => count++)
-      document.addEventListener('dblclick', () => count++)
+      document.addEventListener("click", incriaseCount);
+      document.addEventListener("dblclick", incriaseCountDbl);
+
+      function incriaseCount() {
+        console.log("это я клик");
+        return ++count;
+      }
+      function incriaseCountDbl() {
+        console.log("Это я дабл клик");
+        countDbl++;
+        count -= 2;
+        return countDbl;
+      }
 
       // Отсчет таймера
       for (let i = 0; i <= this.time; i++) {
         setTimeout(() => {
-          timer.innerHTML = `0:0${timeNum}`
-          timeNum-- 
-        }, 1000 * i)
+          timer.innerHTML = `0:0${timeNum}`;
+          timeNum--;
+        }, 1000 * i);
       }
 
       // Происходящее, по истечении таймера
       setTimeout(() => {
-        timer.remove()
-        this.#createClicksCounter(count - 1) // -1 потому, что нажатие на старт таймера идет в счет
-        this.counting = false
-        document.removeEventListener('click', () => count++)
-        document.removeEventListener('dblclick', () => count++)
-      }, 1000 * timeNum)
+        timer.remove();
+        this.#createClicksCounter(count - 1, countDbl); // -1 потому, что нажатие на старт таймера идет в счет
+        this.counting = false;
+        document.removeEventListener("click", incriaseCount);
+        document.removeEventListener("dblclick", incriaseCountDbl);
+      }, 1000 * timeNum);
     }
   }
 
   trigger() {
-    this.#counter()
+    this.#counter();
   }
 }
