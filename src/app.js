b5761af915128804361708d4cc5565923243b9de
Nikita <tonno651@gmail.com>
2022-10-30 18:39:11 +0300

    develop raining cats module

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { BackgroundModule } from './modules/background.module'
 import { ClicksModule } from './modules/clicks.module'
 import { RandomFigureModule } from "./modules/random-figure.module"
 import { CustomMessageModule } from "./modules/custom.message"
+import { RainingCatsModule } from "./modules/raining-cats.module"
 
 const menu = new ContextMenu("#menu")
 
@@ -12,7 +13,8 @@ menu.add(
   new BackgroundModule('bg-color', 'Поменять цвет фона'),
   new ClicksModule('clicks', 'Считать клики', 3),
   new RandomFigureModule("create-random", "Создать случайную фигуру"),
-  new CustomMessageModule('custom-message', 'Создать сообщение', 'Всем привет!')
+  new CustomMessageModule('custom-message', 'Создать сообщение', 'Всем привет!'),
+  new RainingCatsModule('raining-cats', 'Создать дождь из котов', 15)
 )
 
 // Формирование html для контекстного меню
